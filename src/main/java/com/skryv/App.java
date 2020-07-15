f89093954903e026d11bfe297ea1b8196997e236
Matteo Pierro <pierromatteo@gmail.com>
2020-07-15 11:17:04 +0200

    initial commit

---
+++ src/main/java/com/skryv/App.java
@@ -0,0 +1,4 @@
+package com.skryv;
+
+public class App {
+}
---
+++ src/test/java/com/skryv/AppTest.java
@@ -0,0 +1,13 @@
+package com.skryv;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+public class AppTest {
+
+    @Test
+    public void shouldFail() {
+        assertFalse(true);
+    }
+}
